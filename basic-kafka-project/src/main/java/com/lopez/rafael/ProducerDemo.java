91dcd091f568bcce06780b4227d7b2338af0f699
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-20 18:53:44 -0400

    Add Java Producer

--- basic-kafka-project/src/main/java/com/lopez/rafael/ProducerDemo.java
+++ basic-kafka-project/src/main/java/com/lopez/rafael/ProducerDemo.java
@@ -1,7 +1,30 @@
 package com.lopez.rafael;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer(properties);
+
+        // Create ProducerRecord
+        ProducerRecord<String, String> record = new ProducerRecord("first_topic", "hello world");
+
+        // Send data - This is asynchronous, if you don't flush (or close) the producer, the app will finish
+        // before it had a chance to actually send the data
+        producer.send(record);
+        producer.flush();
+        producer.close();
     }
 }
