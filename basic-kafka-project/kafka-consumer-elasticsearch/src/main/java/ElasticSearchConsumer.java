47bda02c218c9c9aca94caac2454c507cbbfe2f7
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-25 17:33:09 -0400

    ElasticSearch + Kafka: Create ElasticSearch client

---
+++ basic-kafka-project/kafka-consumer-elasticsearch/src/main/java/ElasticSearchConsumer.java
@@ -0,0 +1,58 @@
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ElasticSearchConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchConsumer.class);
+
+    public static void main(String[] args) throws IOException {
+        String hostname = System.getProperty("hostname");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        System.out.println(hostname);
+
+        RestHighLevelClient client = createClient(hostname, username, password);
+        String jsonString = "{\"foo\": \"bar\"}";
+
+        // This will fail if there's no 'twitter' index created. Make sure to create it in ElasticSearch
+        IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
+                .source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        LOGGER.info(id);
+
+        client.close();
+    }
+
+    public static RestHighLevelClient createClient(String hostname, String username, String password) {
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        return new RestHighLevelClient(builder);
+    }
+}
