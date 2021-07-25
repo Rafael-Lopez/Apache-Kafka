1f574c8e19665fbba6ddb4e322bcaff0afbcf4b5
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-25 18:30:23 -0400

    ElasticSearch + Kafka: Create Kafka Consumer and send data to ElasticSearch

--- basic-kafka-project/kafka-consumer-elasticsearch/src/main/java/ElasticSearchConsumer.java
+++ basic-kafka-project/kafka-consumer-elasticsearch/src/main/java/ElasticSearchConsumer.java
@@ -4,6 +4,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -15,6 +20,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ElasticSearchConsumer {
@@ -26,17 +34,31 @@ public class ElasticSearchConsumer {
         String password = System.getProperty("password");
 
         RestHighLevelClient client = createClient(hostname, username, password);
-        String jsonString = "{\"foo\": \"bar\"}";
+        KafkaConsumer kafkaConsumer = createKafkaConsumer("twitter_tweets");
 
-        // This will fail if there's no 'twitter' index created. Make sure to create it in ElasticSearch
-        IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
-                .source(jsonString, XContentType.JSON);
+        // Poll for new data
+        while(true) {
+            // When you poll. you need to give it a timout for how long it should try to get data
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        LOGGER.info(id);
+            for(ConsumerRecord<String, String> record: records) {
+                // This will fail if there's no 'twitter' index created. Make sure to create it in ElasticSearch
+                IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
+                        .source(record.value(), XContentType.JSON);
 
-        client.close();
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+                LOGGER.info(id);
+
+                try {
+                    Thread.sleep(1000); // Introducing a small delay
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //client.close();
     }
 
     public static RestHighLevelClient createClient(String hostname, String username, String password) {
@@ -52,5 +74,26 @@ public class ElasticSearchConsumer {
                 });
 
         return new RestHighLevelClient(builder);
+    }
+
+    public static KafkaConsumer<String, String> createKafkaConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+
+        // Create Consumer config
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create Consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer(properties);
+
+        // Subscribe Consumer to our topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        return consumer;
     }
 }
